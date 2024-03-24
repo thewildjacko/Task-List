cf0df439441c905dd613d9a8b66d0e73fe2f3456
Jake Smolowe <jakesmolowe@gmail.com>
2024-03-24 11:06:16 -0700

    README completed and Task List app started

---
+++ Task List/Task List/Task_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ListApp.swift
+//  Task List
+//
+//  Created by Jake Smolowe on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
